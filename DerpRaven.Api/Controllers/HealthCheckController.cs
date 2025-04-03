4b2342599495036a082cd609662bd25d4f8f7623
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-04-03 10:14:04 -0600

    added healthcheck controller

---
+++ DerpRaven.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DerpRaven.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "Healthy" });
+    }
+}
