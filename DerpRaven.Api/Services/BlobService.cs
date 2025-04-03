e194a53af41adb1c42dc330214ae43507546b0c0
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-04-03 00:36:47 -0600

    added a BlobService to the services in the API

---
+++ DerpRaven.Api/Services/BlobService.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Options;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DerpRaven.Api.Services;
+
+public class BlobService
+{
+    private readonly BlobContainerClient _containerClient;
+
+    public BlobService(IOptions<BlobStorageOptions> options)
+    {
+        string connectionString = options.Value.ConnectionString
+            ?? throw new ArgumentNullException("BlobStorage connection string is missing!");
+        string containerName = options.Value.ContainerName;
+        BlobServiceClient _blobClient = new BlobServiceClient(connectionString);
+        _containerClient = _blobClient.GetBlobContainerClient(containerName);
+    }
+
+    public async Task<Azure.Storage.Blobs.Models.BlobContainerInfo> CreateIfNotExistsAsync()
+    {
+        return await _containerClient.CreateIfNotExistsAsync();
+    }
+
+    public async Task<Azure.Storage.Blobs.Models.BlobContentInfo> UploadAsync(string blobName, Stream stream)
+    {
+        var blob = _containerClient.GetBlobClient(blobName);
+        return await blob.UploadAsync(stream, true);
+    }
+
+    public async Task<Azure.Storage.Blobs.Models.BlobDownloadInfo> DownloadAsync(string blobName)
+    {
+        var blob = _containerClient.GetBlobClient(blobName);
+        return await blob.DownloadAsync();
+    }
+
+    public async Task<Response<bool>> DeleteAsync(string blobName)
+    {
+        var blob = _containerClient.GetBlobClient(blobName);
+        return await blob.DeleteIfExistsAsync();
+    }
+}
---
+++ DerpRaven.IntegrationTests.cs/DatabaseServiceTests/ImageServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using DerpRaven.Api.Model;
+using DerpRaven.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Shouldly;
+using DerpRaven.Shared.Dtos;
+using DerpRaven.Api;
+using Microsoft.Extensions.Options;
+namespace DerpRaven.IntegrationTests.cs.DatabaseServiceTests;
+
+public class ImageServiceTests
+{
+    //private ImageService _imageService;
+    //private AppDbContext _context;
+
+    //[SetUp]
+    //public void Setup()
+    //{
+    //    var options = new DbContextOptionsBuilder<AppDbContext>()
+    //        .UseInMemoryDatabase(databaseName: "TestDatabase")
+    //        .Options;
+
+    //    _context = new AppDbContext(options);
+    //    var logger = Substitute.For<ILogger<ImageService>>();
+    //    IOptions<BlobStorageOptions> blobOptions = new();
+    //    _imageService = new ImageService(blobOptions, _context, logger);
+
+    //    _context.SaveChanges();
+    //}
+
+    //[TearDown]
+    //public void TearDownAttribute()
+    //{
+    //    _context.Database.EnsureDeleted();
+    //    _context.Dispose();
+    //}
+}
