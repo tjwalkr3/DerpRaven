cad753dbd4b2fe200a597966875f926a7cd13b59
happy <ricardo_ruizjr@hotmail.com>
2025-04-02 18:17:30 -0600

    Got images in the portfolio pages

--- DerpRaven.Maui/Views/ArtPortfolioPage.xaml.cs
+++ DerpRaven.Maui/Views/ArtPortfolioPage.xaml.cs
@@ -1,9 +1,12 @@
+using DerpRaven.Maui.ViewModels;
+
 namespace DerpRaven.Maui.Views;
 
 public partial class ArtPortfolioPage : ContentPage
 {
-	public ArtPortfolioPage()
+	public ArtPortfolioPage(MainPageViewModel vm)
 	{
 		InitializeComponent();
-	}
+        BindingContext = vm;
+    }
 }
--- DerpRaven.Maui/Views/PlushiePortfolioPage.xaml.cs
+++ DerpRaven.Maui/Views/PlushiePortfolioPage.xaml.cs
@@ -1,9 +1,12 @@
+using DerpRaven.Maui.ViewModels;
+
 namespace DerpRaven.Maui.Views;
 
 public partial class PlushiePortfolioPage : ContentPage
 {
-	public PlushiePortfolioPage()
+	public PlushiePortfolioPage(MainPageViewModel vm)
 	{
 		InitializeComponent();
-	}
+        BindingContext = vm;
+    }
 }
