e6bc884ff156e65eab1138df9c3df6f1b3976a67
happy <ricardo_ruizjr@hotmail.com>
2025-04-04 14:03:11 -0600

    Added some things to OrderHistory

--- DerpRaven.Maui/ViewModels/OrderHistoryPageViewModel.cs
+++ DerpRaven.Maui/ViewModels/OrderHistoryPageViewModel.cs
@@ -1,7 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DerpRaven.Shared.Dtos;
+using System.Collections.ObjectModel;
+
 namespace DerpRaven.Maui.ViewModels;
 
 public partial class OrderHistoryPageViewModel : ObservableObject
 {
+    public ObservableCollection<OrderDto> Orders { get; }
 
+    [ObservableProperty]
+    public bool isExpanded = false;
+
+    public OrderHistoryPageViewModel()
+    {
+        Orders = new ObservableCollection<OrderDto>
+        {
+            new() { Id = 1, Address = "123 Street", Email = "user@example.com", OrderDate = DateTime.Now, UserId = 5, ProductIds = new List<int> { 101, 102 } },
+            new() { Id = 2, Address = "456 Avenue", Email = "test@example.com", OrderDate = DateTime.Now.AddDays(-1), UserId = 6, ProductIds = new List<int> { 103, 104 } }
+        };
+    }
+
+    [RelayCommand]
+    private void ToggleExpand()
+    {
+        IsExpanded = !IsExpanded;
+    }
 }
