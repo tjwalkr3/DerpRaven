2fb095cdddec30b680cb8589edf679e660aa27ba
Thomas Jones <tjwalkr3@gmail.com>
2025-04-04 23:20:53 -0600

    cut down azure deployment time by doing it in parallel

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -22,56 +22,56 @@ CONTAINER_NAME_2="$RESOURCE_GROUP-web"
 # Create the container service for the API container
 #####################################################
 (
-az container delete \
-    --name "$CONTAINER_NAME_1" \
-    --resource-group "$RESOURCE_GROUP" \
-    --yes
+    az container delete \
+        --name "$CONTAINER_NAME_1" \
+        --resource-group "$RESOURCE_GROUP" \
+        --yes
 
-az container create \
-    --resource-group "$RESOURCE_GROUP" \
-    --name "$CONTAINER_NAME_1" \
-    --image "$IMAGE1" \
-    --dns-name-label "$CONTAINER_NAME_1" \
-    --ports "8080" \
-    --os-type Linux \
-    --cpu 1 \
-    --memory 1.5 \
-    --registry-login-server "index.docker.io" \
-    --registry-username "$REGISTRY_USER" \
-    --registry-password "$REGISTRY_PASS" \
-    --location "$LOCATION" \
-    --output none \
-    --secure-environment-variables \
-        ConnectionStrings__DefaultConnection="$DB_CONN_STRING" \
-        BlobStorage__ConnectionString="$BLOB_CONN_STRING" \
-        BlobStorage__ContainerName="$BLOB_CONTAINER_NAME" \
-    || { echo "Failed to create container $CONTAINER_NAME_1"; exit 1; }
+    az container create \
+        --resource-group "$RESOURCE_GROUP" \
+        --name "$CONTAINER_NAME_1" \
+        --image "$IMAGE1" \
+        --dns-name-label "$CONTAINER_NAME_1" \
+        --ports "8080" \
+        --os-type Linux \
+        --cpu 1 \
+        --memory 1.5 \
+        --registry-login-server "index.docker.io" \
+        --registry-username "$REGISTRY_USER" \
+        --registry-password "$REGISTRY_PASS" \
+        --location "$LOCATION" \
+        --output none \
+        --secure-environment-variables \
+            ConnectionStrings__DefaultConnection="$DB_CONN_STRING" \
+            BlobStorage__ConnectionString="$BLOB_CONN_STRING" \
+            BlobStorage__ContainerName="$BLOB_CONTAINER_NAME" \
+        || { echo "Failed to create container $CONTAINER_NAME_1"; exit 1; }
 ) &
 
 #####################################################
 # Create the container service for the Web container
 #####################################################
 (
-az container delete \
-    --name "$CONTAINER_NAME_2" \
-    --resource-group "$RESOURCE_GROUP" \
-    --yes
+    az container delete \
+        --name "$CONTAINER_NAME_2" \
+        --resource-group "$RESOURCE_GROUP" \
+        --yes
 
-az container create \
-    --resource-group "$RESOURCE_GROUP" \
-    --name "$CONTAINER_NAME_2" \
-    --image $IMAGE2 \
-    --dns-name-label "$CONTAINER_NAME_2" \
-    --ports "80" \
-    --os-type Linux \
-    --cpu 1 \
-    --memory 1.5 \
-    --registry-login-server "index.docker.io" \
-    --registry-username "$REGISTRY_USER" \
-    --registry-password "$REGISTRY_PASS" \
-    --location "$LOCATION" \
-    --output none \
-    || { echo "Failed to create container $CONTAINER_NAME_1"; exit 1; }
+    az container create \
+        --resource-group "$RESOURCE_GROUP" \
+        --name "$CONTAINER_NAME_2" \
+        --image $IMAGE2 \
+        --dns-name-label "$CONTAINER_NAME_2" \
+        --ports "80" \
+        --os-type Linux \
+        --cpu 1 \
+        --memory 1.5 \
+        --registry-login-server "index.docker.io" \
+        --registry-username "$REGISTRY_USER" \
+        --registry-password "$REGISTRY_PASS" \
+        --location "$LOCATION" \
+        --output none \
+        || { echo "Failed to create container $CONTAINER_NAME_1"; exit 1; }
 ) &
 
 wait
