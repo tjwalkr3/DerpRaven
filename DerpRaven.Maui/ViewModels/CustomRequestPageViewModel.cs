1994dd3c9c9413021409b74653ff6898fd79704e
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-04-08 17:23:28 -0600

    added the all customrequests view

--- DerpRaven.Maui/ViewModels/CustomRequestPageViewModel.cs
+++ DerpRaven.Maui/ViewModels/CustomRequestPageViewModel.cs
@@ -1,7 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DerpRaven.Shared.ApiClients;
+using DerpRaven.Shared.Dtos;
+using System.Collections.ObjectModel;
 namespace DerpRaven.Maui.ViewModels;
 
-public partial class CustomRequestPageViewModel : ObservableObject
+public partial class CustomRequestPageViewModel(CustomRequestClient client) : ObservableObject
 {
+    private ObservableCollection<CustomRequestDto> CustomRequests;
 
+    [RelayCommand]
+    public async Task GetCustomRequests()
+    {
+        // See if we can get all custom requests
+        List<CustomRequestDto>? requests = await client.GetCustomRequestsByUserEmailAsync();
+        // If we don't find any, we don't need to do much, just return, leaving the list empty
+        if (requests == null) return;
+        // If we do find some, we need to set the list to include the requests
+        CustomRequests = new(requests);
+    }
 }
