746d28523c54e1364273fca6cb7aa9bad313de8d
happy <ricardo_ruizjr@hotmail.com>
2025-04-18 02:13:29 -0600

    Did some ghosting on the blazor wasm

---
+++ DerpRaven.Blazor/Pages/AddProducts.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace DerpRaven.Blazor.Pages;
+
+public partial class AddProducts
+{
+    private string ProductName { get; set; }
+    private string Price { get; set; }
+    private string Description { get; set; }
+    private bool IsPlush { get; set; }
+    private bool IsArt { get; set; }
+
+    private void UploadPhoto()
+    {
+        // You can integrate file upload logic here
+    }
+
+    private void AddProduct()
+    {
+        // Submit logic here
+    }
+}
