5ade7fc8a2b909220398a35dff739dd105ce4ea3
Derpipose <thefluffmaster2000@gmail.com>
2025-04-21 10:43:31 -0600

    Made the MAUI test project added the project dependencies and nsubstitute

---
+++ DerpRaven.MauiTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace DerpRaven.MauiTests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
