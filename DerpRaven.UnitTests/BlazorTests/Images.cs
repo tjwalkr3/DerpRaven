6cee49af5d0cb4fc734c3f5ca3abefb765bf7f9f
Derpipose <thefluffmaster2000@gmail.com>
2025-04-23 10:54:46 -0600

    Added blazor project for testing

---
+++ DerpRaven.UnitTests/BlazorTests/Images.cs
@@ -0,0 +1,15 @@
+﻿namespace DerpRaven.UnitTests;
+
+public class Images
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
